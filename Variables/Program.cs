73903ff7dfae6e04f26b4a5c0184ce1817fed7d7
Z M Asif Jamil <80680790+FurySimo@users.noreply.github.com>
2021-03-16 15:27:22 +0600

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Asif";
+            int myage = 22;
+            char grade = 'A';
+            double score = 3.5;
+            Console.WriteLine("I am " + myname +".");
+            Console.WriteLine("I am " + myage + " years old.");
+            Console.WriteLine("I got " + grade + " grade in BSC.");
+            Console.WriteLine("My GPA is " + score + ".");
+            
+            
+
+
+        }
+    }
+}
